4dc2100b78ca992f962685f53e7d1bcc234a8d12
Volodimir1 <vladimirn062012@gmail.com>
2021-10-19 01:43:46 +0300

    f-3-events

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
+'use strict'
 // document.getElementById("app").innerHTML = `
 // <h1>Hello Vanilla!</h1>
+//mousedown / mouseup
+function play(event) {
+  console.log(event.type + "; " + event.currentTarget);
+};
+function stop(event) {
+  console.log(event.type);
+};
+keyboard.addEventListener("mousedown", play);
+keyboard.addEventListener("mouseup", stop);
