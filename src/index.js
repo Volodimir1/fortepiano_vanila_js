a7c9abb5b4f9e908ae71d97be8c22af7874b9571
Volodimir1 <vladimirn062012@gmail.com>
2021-10-19 02:16:29 +0300

    f-6-js-target-li

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@
 // document.getElementById("app").innerHTML = `
 // <h1>Hello Vanilla!</h1>
 //mousedown / mouseup
-function play(event) {
-  console.log(event.type + "; " + event.currentTarget);
-};
-function stop(event) {
-  console.log(event.type);
-};
-keyboard.addEventListener("mousedown", play);
-keyboard.addEventListener("mouseup", stop);
+
+// function play(event) {
+//   console.log(event.type + "; " + event.currentTarget);
+// };
+// function stop(event) {
+//   console.log(event.type);
+// };
+// keyboard.addEventListener("mousedown", play);
+// keyboard.addEventListener("mouseup", stop);
+
+var $ul = document.querySelector('ul');
+
+$ul.addEventListener('click', function(e) {
+   if (e.target.tagName === 'LI') {
+      e.target.classList.toggle('done');
+      console.log(e.target.classList);
+      console.log(e.target.innerHTML);
+   }
+});
