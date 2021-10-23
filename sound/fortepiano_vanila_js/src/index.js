33411c3ad60ec53de97556ae63bb58de3d05a531
Volodimir1 <61084049+Volodimir1@users.noreply.github.com>
2021-10-23 18:52:21 +0300

    index.js

---
+++ sound/fortepiano_vanila_js/src/index.js
@@ -0,0 +1,36 @@
+'use strict'
+
+function play(e) {
+  e.target.classList.toggle('list__key--pressed');
+//   sound(e.target.innerHTML);
+};
+function stop(e) {
+  e.target.classList.toggle('list__key--pressed');
+};
+// function sound(note) {
+//    var audio = new Audio();
+//    audio.currentTime = 0;
+//    audio.src = `sound/${note}2.mp3`;
+//    audio.autoplay = true;
+//  };
+
+var $ul = document.querySelector('ul');
+// MOBIL TOUCH
+var audio = new Audio();
+$ul.addEventListener('touchstart', function(e) {
+  event.preventDefault();
+  if (e.target.tagName === 'LI') {
+     play(e);
+//     var audio = new Audio();
+     audio.currentTime = 0;
+     audio.src = `sound/${e.target.innerHTML}2.mp3`;
+     audio.autoplay = true;
+  }
+});
+$ul.addEventListener('touchend', function(e) {
+  event.preventDefault();
+  if (e.target.tagName === 'LI') {
+    stop(e);
+    audio.pause();
+  }
+});
