e69d8d1fd26b3df41e0118a9720282e682ee4fc1
Volodimir1 <vladimirn062012@gmail.com>
2021-11-14 18:56:19 +0200

    29-favicon

--- src/index.js
+++ src/index.js
@@ -2,21 +2,12 @@
 
 function press(e) {
   e.target.classList.toggle(e.target.classList + '_pressed');
-//   sound(e.target.innerHTML);
 };
 function unpress(e) {
-//  e.target.classList.toggle(/.+pressed/);
   e.target.classList.remove('list__key_white_pressed', 'list__key_black_pressed');
 };
-// function sound(note) {
-//    var audio = new Audio();
-//    audio.currentTime = 0;
-//    audio.src = `sound/${note}2.mp3`;
-//    audio.autoplay = true;
-//  };
 
 var $ul = document.querySelector('ul');
-// var audio = new Audio(); //single sound(
 // MOBIL TOUCH
 $ul.addEventListener('touchstart', function(e) {
   event.preventDefault();
@@ -29,9 +20,7 @@ $ul.addEventListener('touchstart', function(e) {
   }
 });
 $ul.addEventListener('touchend', function(e) {
-  // event.preventDefault(); //?
   if (e.target.tagName === 'LI') {
      unpress(e);
-   //   audio.paused ? audio.play() : audio.pause(); //single sound(
   }
 });
